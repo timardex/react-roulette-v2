2da4e4814fcff02a176800a7928508f4ebdb9fbf
csaba <timardex@gmail.com>
2021-10-11 14:48:13 +0200

    fix linebets component

--- src/components/TheTable/LineBets/index.js
+++ src/components/TheTable/LineBets/index.js
@@ -8,9 +8,13 @@ const LineBets = (props) => {
   return (
     <div className={`line-bets ${data.className}`}>
       <div className="form">
-        <label className={"form-label"} htmlFor={data.id} title={`${data.className}: ${data.numbers}`}>
+        <label
+          className={"form-label"} htmlFor={data.id}
+          title={`${data.className.includes('Split') ? 'split' : data.className}: ${data.numbers}`}>
+
           <input className="form-input" type="checkbox" id={data.id} value={data.id}/>
           {data.checked && <span className="chip"></span>}
+          
         </label>
       </div>
     </div>
